e45e2e094aa52878e7ac708e0021a686615f54b0
everton <evertonds@gmail.com>
2023-01-27 22:02:15 -0300

    ok

--- aplicacao/main.py
+++ aplicacao/main.py
@@ -1,5 +1,8 @@
+from typing import List
 from fastapi import FastAPI 
 from fastapi import HTTPException, status
+from pydantic import BaseModel
+from models import Alunos
 
 
 app = FastAPI()
@@ -15,18 +18,31 @@ alunos = {
     4: "Thiago"
 }
 
+alunos = [
+    {"id": 1, "name": "William" }
+]
+
+class Aluno(BaseModel):
+    id: int
+    name: str
+
+
 @app.get('/alunos')
 async def get_alunos(): 
     return alunos
 
 @app.get('/alunos/{aluno_id}')
-async def get_aluno(aluno_id: int): 
-    aluno = alunos[aluno_id]
-    alunos.update({
-        'id': aluno_id
-        })
-    return aluno
+async def get_aluno(aluno_id: int) -> List[Aluno]:
+    try:
+        aluno = alunos[aluno_id]
+        alunos.update({
+            'id': aluno_id
+            })
+        
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Aluno não encontrado')
        
+    return aluno
 
 
 if __name__ == '__main__':
@@ -39,3 +55,34 @@ if __name__ == '__main__':
         log_level = "info",
         reload = True
     )
+
+
+professores = {
+    1: {'nome': 'Everton', 'idade': 37, 'e-mail': 'e@localhost'},
+    2: {"nome": "Andre", "idade": 23, "e-mail": "a@localhost"},
+    3: {"nome": "William", "idade": 28, "e-mail": "w@localhost"}
+}
+
+
+@app.get('/professores')
+async def get_professores(): 
+    return professores
+
+@app.get('/professoresNome/{prof_nome}')
+async def get_professor_por_chave(prof_nome: str):
+    for professor in professores.values():
+        if professor['nome'] == prof_nome:
+            return professor
+    
+
+@app.post("/aluno/")
+async def post_aluno(aluno: Aluno):
+    next_id = int(len(aluno))
+    aluno = {
+        "next_id" : 0,
+        "nome" : "Everton",
+        "idade" : 38,
+        "email" : "evert@gmail.com"
+    }
+
+    return aluno
